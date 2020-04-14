a9efbd36651828a84cd786ec83c430ee97b5b468
Meghouche Rafik <rafikmeghouche@gmail.com>
2020-04-14 23:40:48 +0100

    About page

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-get('about' , function () {
-
+get('abut' , function () {
+   return 'About page';
 });
